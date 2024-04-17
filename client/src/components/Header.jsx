b6e929efbd5486eff4b949542e28b17dacd69f45
Techie Mike <mngene4@gmail.com>
2024-04-17 23:20:38 +0100

    Added Dark Mode Funtionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon } from 'react-icons/fa';
-import { useSelector } from 'react-redux';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleTheme } from '../app/theme/themeSlice';
 
 export default function Header() {
     const path = useLocation().pathname;
-    const {currentUser} = useSelector(state => state.user);
+    const dispatch = useDispatch();
+    const {currentUser} = useSelector((state) => state.user);
+    const {theme} = useSelector((state) => state.theme);
   return (
     <Navbar className='border-b-2'>
         <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -26,8 +29,8 @@ export default function Header() {
             <AiOutlineSearch />
         </Button>
         <div className="flex gap-2 md:order-2">
-            <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
-                <FaMoon/>
+            <Button className='w-12 h-10 hidden sm:inline' color='gray' pill onClick={()=>dispatch(toggleTheme())}>
+                {theme === 'light' ? (<FaMoon/>) : (<FaSun/>)}
             </Button>
             {currentUser ? (
                 <Dropdown 
@@ -37,7 +40,10 @@ export default function Header() {
                         <Avatar
                             alt='user'
                             img={currentUser.profilePicture}
+                            bordered
                             rounded
+                            status="online" 
+                            statusPosition="bottom-right"
                         />
                     }
                 >
