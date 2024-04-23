a37d8d387cb79894c7ce11f947302a71fdb44c28
Techie Mike <mngene4@gmail.com>
2024-04-23 11:33:13 +0100

    Add Admin to the User Scheme

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -31,9 +31,7 @@ export const signin = async (req, res, next) => {
 
     if (!email || !password || email === '' || password === '') {
         next(errorHandler(400, 'All Fields are required and must not be Empty'));
-    }
-
-    const hashedPassword = bcryptjs.hashSync(password, 10);
+    } 
 
     try {
         const validUser = await User.findOne({ email });
@@ -47,7 +45,7 @@ export const signin = async (req, res, next) => {
         }
 
         const token = jwt.sign(
-            { userId: validUser._id }, process.env.JWT_SECRET_KEY, { expiresIn: '1d' }
+            { userId: validUser._id, isAdmin: validUser.isAdmin }, process.env.JWT_SECRET_KEY, { expiresIn: '1d' }
         );
 
         const { password: pass, ...rest } = validUser._doc;
@@ -66,7 +64,7 @@ export const googleAuth = async (req, res, next) => {
     try {
         const user = await User.findOne({email});
         if (user) {
-            const token = jwt.sign({id: user._id}, process.env.JWT_SECRET_KEY);
+            const token = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.JWT_SECRET_KEY);
             const {password, ...rest} = user._doc;
             res.status(200).cookie('access_token', token, {
                 httpOnly: true,
@@ -81,7 +79,7 @@ export const googleAuth = async (req, res, next) => {
                 profilePicture: googlePhotoUrl,
             });
             await newUser.save();
-            const token = jwt.sign({id: user._id}, process.env.JWT_SECRET_KEY);
+            const token = jwt.sign({id: user._id, isAdmin: newUser.isAdmin}, process.env.JWT_SECRET_KEY);
             const {password, ...rest} = user._doc;
             res.status(200).cookie('access_token', token, {
                 httpOnly: true,
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -21,6 +21,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: "https://cdn.pixabay.com/photo/2018/11/13/21/43/avatar-3814049_1280.png",
     },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
 }, {timestamps: true});
 
 const User = mongoose.model('User', userSchema);
